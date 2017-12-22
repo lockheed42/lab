3ca41d46749fdfc463313eec5ee0309b8fe1750d
lockheed42 <hongjiexxx@126.com>
2017-12-22 15:54:28 +0800

    tmp

---
+++ python_lab/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+'test'
+__author__ = 'lockheed'
+
+
+def trim(string):
+    left = 0
+    right = -1
+    length = len(string)
+    for i in range(length):
+        if string[i:i + 1] == ' ':
+            print(left)
+            left += 1
+        else:
+            break
+    for i in range(length):
+        if string[-1:] == ' ':
+            right = -1
+    return string[left:right]
+
+
+string = '   abbbcccd'
+print(trim(string))
