8c38b7f130922339c6deecd7b2efa72b52cf39c5
lockheed42 <hongjiexxx@126.com>
2017-12-26 18:17:13 +0800

    tmp

--- python_lab/test.py
+++ python_lab/test.py
@@ -17,9 +17,21 @@ def trim(string):
             break
     for i in range(length):
         if string[-1:] == ' ':
-            right = -1
+            right -= -1
     return string[left:right]
 
 
-string = '   abbbcccd'
-print(trim(string))
+if trim('hello  ') != 'hello':
+    print('测试失败1!')
+elif trim('  hello') != 'hello':
+    print('测试失败2!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败3!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败4!')
+elif trim('') != '':
+    print('测试失败5!')
+elif trim('    ') != '':
+    print('测试失败6!')
+else:
+    print('测试成功!')
