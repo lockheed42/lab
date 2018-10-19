7ebe0be06d1ab5723db6d27dd5de7409f3255b1e
lockheed42 <hongjiexxx@126.com>
2018-10-19 16:57:13 +0800

    海龟计划启动

--- python_lab/spider/spider.py
+++ python_lab/spider/spider.py
@@ -15,42 +15,37 @@ from datetime import datetime
 import os
 import redis
 import traceback
-import time as time_machine
+import time
 from multiprocessing import Pool
 import multiprocessing
 
-'''
-记录错误日志
-'''
 
-
-def error_log(e):
+def error_log(sub_id):
+    """记录错误日志"""
     global host
     global error_log_path
     # TODO 日志与html保存路径可配置
     with open(error_log_path + "/" + get_domain(host) + "_error.log", 'a') as f:
-        f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '  ' + str(os.getpid()) + '  ' + str(traceback.format_exc()) + '\n')
-
-
-'''
-其他日志
-'''
+        f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '  ' + str(sub_id) + '  ' + str(
+            traceback.format_exc()) + '\n')
 
 
 def log(file, content):
+    """其他日志"""
     global host
     global log_path
     with open(log_path + "/" + get_domain(host) + "_" + file + ".log", 'a') as f:
         f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '  ' + str(content) + '\n')
 
 
-'''
-html源文件保存
-获取html要写入的文件路径，不存在则会创建
-'''
-
-
 def save_html(url, content):
+    """
+    html源文件保存
+    获取html要写入的文件路径，不存在则会创建
+    :param url:
+    :param content:
+    :return:
+    """
     global html_res_path
     url = get_domain(url)
     url = url.strip('/')
@@ -67,8 +62,8 @@ def save_html(url, content):
         file = url.replace('/', '_')
     else:
         root_path = url[:index + suffix_len]
-        if not os.path.exists(html_res_path + "/" +root_path):
-            os.mkdir(html_res_path + "/" +root_path)
+        if not os.path.exists(html_res_path + "/" + root_path):
+            os.mkdir(html_res_path + "/" + root_path)
         dir_path = url[index + suffix_len + 1:]
         # 根目录
         if dir_path == '':
@@ -76,7 +71,7 @@ def save_html(url, content):
 
         # 迭代创建目录
         dir_path_array = dir_path.split('/')
-        tmp_dir = html_res_path + "/" +root_path
+        tmp_dir = html_res_path + "/" + root_path
         for dir_block in dir_path_array:
             if dir_block == dir_path_array[-1]:
                 # 添加文件后缀，防止文件与目录重名
@@ -96,44 +91,47 @@ def save_html(url, content):
         f.write(content)
 
 
-'''
-判断link是否已经抓取过。抓取过返回 False
-'''
-
-
-def url_is_get(url):
+def set_url_catch(url):
+    """判断link是否已经抓取过。抓取过返回 False"""
     global redis_connect
-
-    is_get = redis_connect.setnx("catch_url:" + url, 1)
+    is_get = redis_connect.set("catch_url:" + url, 1)
     return True if is_get else False
 
 
-'''
-推入url等待抓取队列
-key结构：url_queue:{host}:{deep} = url
-'''
+def is_url_catch(url):
+    """判断url是否被抓去过。抓取过返回 False"""
+    global redis_connect
+    rs = redis_connect.get("catch_url:" + url)
+    return False if rs else True
 
 
 def url_push(host, url, deep):
+    """
+    推入url等待抓取队列
+    key结构：url_queue:{host}:{deep} = url:param host:
+    :param url:
+    :param deep: 
+    :return:
+    """
     global redis_connect
     queue_key = "url_queue:" + host + ":" + str(deep)
-    rs = redis_connect.lpush(queue_key, url)
-
-
-'''
-从队列获取要抓取的url
-辅助key，表达当前哪个深度的队列有数据待处理
-    url_deep:{host} = deep
-如果当前队列pop为None，辅助key+1，表示下次从更深的队列获取数据
-返回值：url, deep
-'''
+    redis_connect.lpush(queue_key, url)
 
 
 def url_pop(host):
+    """
+    从队列获取要抓取的url
+    辅助key，表达当前哪个深度的队列有数据待处理
+    url_deep:{host} = deep
+    如果当前队列pop为None，辅助key+1，表示下次从更深的队列获取数据
+    返回值：url, deep
+
+    :param host:
+    :return:
+    """
     global redis_connect
     url_deep_key = "url_deep:" + host
     deep = redis_connect.get(url_deep_key)
-    deep = deep.decode('utf-8')
     queue_key = "url_queue:" + host + ":" + deep
     pop_url = redis_connect.rpop(queue_key)
     # 如果当前深度队列 已取完，从下一级深度读取
@@ -143,36 +141,22 @@ def url_pop(host):
         queue_key = "url_queue:" + host + ":" + str(deep)
         pop_url = redis_connect.rpop(queue_key)
 
-    if pop_url is not None:
-        pop_url = pop_url.decode('utf-8')
-
     return pop_url, int(deep)
 
 
-'''
-获取 url 的 domain部分
-'''
-
-
 def get_domain(url):
-    return url.strip('/').strip('https://').strip('http://')
-
-
-'''
-获取域名后缀
-'''
+    """获取 url 的 domain部分"""
+    return url.strip('/').lstrip('https://')
 
 
 def get_domain_suffix():
+    """获取域名后缀"""
     return ['.com', '.cn', '.net', '.wang', '.tv', '.org', '.cc']
 
 
-'''
-获取主域名，包括http部分的前缀
-'''
-
-
 def get_host(url):
+    """获取主域名，包括http部分的前缀"""
+    url = url.strip(" ").strip("\n")
     index = -1
     suffix_len = 0
     for suffix in get_domain_suffix():
@@ -187,15 +171,14 @@ def get_host(url):
         return url[:index + suffix_len]
 
 
-'''
-请求程序，模拟真实用户
-还需要优化
-
-响应程序，处理相应数据以及模拟处理
-'''
-
-
 def request_parse(url):
+    """
+    请求程序，模拟真实用户
+    还需要优化
+    响应程序，处理相应数据以及模拟处理
+    :param url:
+    :return:
+    """
     # TODO header可配置
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
     accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
@@ -226,71 +209,67 @@ def request_parse(url):
     # 读取内容
     res = response.read()
     # TODO 编码可配置
-    return res.decode('gbk'), content_length
+    return res.decode('utf-8'), content_length
+
 
 '''
 插入数据库，暂时搁置
 '''
+
+
 # def insert():
-    # connection = pymysql.connect(host='localhost',
-    #                              user='root',
-    #                              password='',
-    #                              db='test',
-    #                              port=3306,
-    #                              charset='utf8')
-    #
-    # if deep == 0:
-    #     # 插入首页html
-    #     pid = 0
-    #     sql_main = "INSERT INTO t_html (`domain`,`current_url`, `pid`, `deep`, `cdate`) VALUES "
-    #     sql_main = sql_main + "('" + get_domain(host) + "','" + url + "'," + str(pid) + ", '" + str(
-    #         deep) + "','" + time + "')"
-    #
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(sql_main)
-    #         connection.commit()
-    # else:
-    #     # 查询当前url的id
-    #     sql_main = "select * from t_html where `current_url` = '" + url + "'"
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(sql_main)
-    #         data = cursor.fetchone()
-    #         connection.commit()
-    #     if data is not None:
-    #         pid = data[0]
-    #     else:
-    #         pid = -1
-    #         log('skip', '  在数据库被未插入  ' + str(url))
-    #
-    # # 插入当前html下的子链接
-    # sql = "INSERT INTO t_html (`domain`,`current_url`, `pid`, `deep`, `cdate`) VALUES "
-    # for ele in href:
-    #     if ele is not None and ele[:4] == 'http':
-    #         sql = sql + "('" + get_domain(host) + "','" + ele + "','" + str(pid) + "','" + str(
-    #             deep + 1) + "','" + time + "'),"
-    # sql = sql.rstrip(',')
-    #
-    # with connection.cursor() as cursor:
-    #     cursor.execute(sql)
-    #     connection.commit()
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              db='test',
+#                              port=3306,
+#                              charset='utf8')
+#
+# if deep == 0:
+#     # 插入首页html
+#     pid = 0
+#     sql_main = "INSERT INTO t_html (`domain`,`current_url`, `pid`, `deep`, `cdate`) VALUES "
+#     sql_main = sql_main + "('" + get_domain(host) + "','" + url + "'," + str(pid) + ", '" + str(
+#         deep) + "','" + ctime + "')"
+#
+#     with connection.cursor() as cursor:
+#         cursor.execute(sql_main)
+#         connection.commit()
+# else:
+#     # 查询当前url的id
+#     sql_main = "select * from t_html where `current_url` = '" + url + "'"
+#     with connection.cursor() as cursor:
+#         cursor.execute(sql_main)
+#         data = cursor.fetchone()
+#         connection.commit()
+#     if data is not None:
+#         pid = data[0]
+#     else:
+#         pid = -1
+#         log('skip', '  在数据库被未插入  ' + str(url))
+#
+# # 插入当前html下的子链接
+# sql = "INSERT INTO t_html (`domain`,`current_url`, `pid`, `deep`, `cdate`) VALUES "
+# for ele in href:
+#     if ele is not None and ele[:4] == 'http':
+#         sql = sql + "('" + get_domain(host) + "','" + ele + "','" + str(pid) + "','" + str(
+#             deep + 1) + "','" + ctime + "'),"
+# sql = sql.rstrip(',')
+#
+# with connection.cursor() as cursor:
+#     cursor.execute(sql)
+#     connection.commit()
 
 
-'''
-抓取程序
-'''
-
-
-def catch(url, deep, redis_connect):
+def catch(url, deep, sub_id):
+    """抓取程序"""
     global deep_limit
     try:
-        # 基础信息
-        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
         # 降低抓取速度，减少访问压力 TODO 多进程正式抓取时开启
-        # time_machine.sleep(0.5)
+        # time.sleep(0.5)
 
         # 阻止重复抓取
-        if url_is_get(url) == False:
+        if is_url_catch(url) == False:
             return True
 
         # 抓取数据
@@ -299,10 +278,10 @@ def catch(url, deep, redis_connect):
 
         # 把html文件写入。存入html路径下
         save_html(url, content)
+        set_url_catch(url)
         # 记录当前正在抓取的url
         log('run',
-            str(os.getpid()) + '  ' + url + '  ' + str(content_length) + 'byte  ' + str(real_length) + 'byte  ' + str(
-                deep))
+            str(sub_id) + '  ' + url + '  ' + str(content_length) + 'byte  ' + str(real_length) + 'byte  ' + str(deep))
 
         # 链接列表
         href = []
@@ -331,30 +310,28 @@ def catch(url, deep, redis_connect):
             # 继续抓取同一域名下的资源
             if get_host(ele) == host:
                 # 阻止已抓取的页面丢入队列
-                if url_is_get(ele) == False:
+                if is_url_catch(ele) is False:
                     continue
                 url_push(host, ele, deep + 1)
             else:
                 # TODO 非同一域名另外保存根目录
-                log('skip', str(os.getpid()) + '  ' + '  非同一域名  ' + str(host) + '  ' + str(ele))
+                log('skip', str(sub_id) + '  ' + '  非同一域名  ' + str(host) + '  ' + str(ele))
         return True
     except BaseException as e:
-        error_log(e)
+        error_log(sub_id)
         return True
 
 
-'''
-爬取子进程
-'''
-def sub_process(pipe):
+def sub_process(pipe, sub_id):
+    """爬取子进程"""
     # 为每次子进程提供一个连接池
-    redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
+    redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)
     redis_connect = redis.Redis(connection_pool=redis_pool)
 
     is_continue = True
     while is_continue:
         message = pipe.recv()
-        log('pipe_recv', str(os.getpid()) + '  ' + message)
+        log('pipe_recv', str(sub_id) + '  ' + message)
         # 收到结束信号中止循环
         if message == '---end---':
             is_continue = False
@@ -363,53 +340,94 @@ def sub_process(pipe):
         url = message_array[0]
         deep = int(message_array[1])
 
-        catch(url, deep, redis_connect)
-        redis_connect.lpush('free_process:' + host, str(os.getpid()))
+        catch(url, deep, sub_id)
+        redis_connect.lpush('free_process:' + host, str(sub_id))
+
+
+def do_cmd(cmd):
+    """执行接收的命令"""
+    print(cmd)
+    switcher = {
+        "stop": end_program,
+    }
+    return switcher.get(cmd, '')() if callable(switcher.get(cmd, '')) else print('命令错误')
+
+
+def end_program():
+    """结束程序，发送信号给子程序，并等待子进程结束"""
+    global process_num
+    global pipe_pool
+    global process_pool
+    for j in range(process_num):
+        pipe_pool[j][0].send('---end---')
+
+    process_pool.close()
+    process_pool.join()
+    print('All url done.')
+    exit()
+
+
+def init_redis(host):
+    """初始化一个域名下的redis数据"""
+    global redis_connect
+    redis_connect.flushall()
+    redis_connect.delete('free_process:' + host)
+    for deep in range(20):
+        redis_connect.delete("url_queue:" + host + ":" + str(deep))
+    return
 
 
 '''
 main
 '''
 # # 抓取深度限制
-# deep_limit = 6
+# deep_limit = 10
 # # 进程数
-# process_num = 50
+# process_num = 60
 # # 下载的html资源文件保存地址
 # html_res_path = '/Library/WebServer/Documents/code/spider_res/html'
 # # 日志文件保存地址
 # log_path = '/Library/WebServer/Documents/code/spider_res/log'
 # error_log_path = '/Library/WebServer/Documents/code/spider_res/log/error'
+# host = 'https://www.heavengifts.com'
+# cmd_file = './cmd.txt'
 #
 # # 重置redis缓存
-# redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
+# redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)
 # redis_connect = redis.Redis(connection_pool=redis_pool)
 #
 # # 初始化 url队列
-# redis_connect.flushdb()
+# init_redis(host)
 # redis_connect.set('url_deep:' + host, 0)
-# url_push(host, host, 0)
+# url_push(host, host, '0')
 #
 # # 多工作进程运行
 # process_pool = Pool(process_num)
 # pipe_pool = {}
 # for i in range(process_num):
 #     # 重要，延长每个pipe创建的间隔
-#     time_machine.sleep(0.5)
+#     time.sleep(0.5)
 #     pipe_pool[i] = multiprocessing.Pipe()
-#     process_pool.apply_async(sub_process, args=(pipe_pool[i][1],))
+#     process_pool.apply_async(sub_process, args=(pipe_pool[i][1], i,))
 #     # 设置空闲进程队列
-#     redis_connect.lpush('free_process:' + host, '1')
+#     redis_connect.lpush('free_process:' + host, i)
 #
 # # 抓取根目录
 # url, deep = url_pop(host)
 # pipe_pool[0][0].send(url + '------' + str(deep))
 # redis_connect.brpop('free_process:' + host)
-# time_machine.sleep(10)
+# time.sleep(10)
 #
 # # 主进程逻辑
-# i = 0
 # while True:
-#     redis_connect.brpop('free_process:' + host)
+#     if os.path.exists(cmd_file):
+#         with open(cmd_file, 'r') as f:
+#             cmd = f.readline().strip()
+#             do_cmd(cmd)
+#
+#         os.remove(cmd_file)
+
+#     sub_id = redis_connect.brpop('free_process:' + host)[1]
 #     url, deep = url_pop(host)
 #     if url is None:
 #         # 任务全部做完，发送信号给子程序，并等待子进程结束
@@ -421,58 +439,54 @@ main
 #         print('All url done.')
 #         exit()
 #
-#     pipe_num = i % process_num
 #     log('pipe_send', url + '------' + str(deep))
-#     pipe_pool[pipe_num][0].send(url + '------' + str(deep))
-#     i = i + 1
-
+#     pipe_pool[int(sub_id)][0].send(url.strip("\n") + '------' + str(deep))
 
 '''
 临时方案，异常中止之后的继续采集
 '''
 # 抓取深度限制
-deep_limit = 6
+deep_limit = 10
 # 进程数
-process_num = 40
+process_num = 10
 # 下载的html资源文件保存地址
 html_res_path = '/Library/WebServer/Documents/code/spider_res/html'
 # 日志文件保存地址
 log_path = '/Library/WebServer/Documents/code/spider_res/log'
 error_log_path = '/Library/WebServer/Documents/code/spider_res/log/error'
 
-host = "http://www.biqugezw.com"
+host = "https://www.heavengifts.com"
+cmd_file = './cmd.txt'
 
 # 重置redis缓存
-redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
+redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)
 redis_connect = redis.Redis(connection_pool=redis_pool)
+redis_connect.delete('free_process:' + host)
 
 # 多工作进程运行
 process_pool = Pool(process_num)
 pipe_pool = {}
 for i in range(process_num):
     # 重要，延长每个pipe创建的间隔
-    time_machine.sleep(0.5)
+    time.sleep(0.5)
     pipe_pool[i] = multiprocessing.Pipe()
-    process_pool.apply_async(sub_process, args=(pipe_pool[i][1],))
+    process_pool.apply_async(sub_process, args=(pipe_pool[i][1], i,))
     # 设置空闲进程队列
-    redis_connect.lpush('free_process:' + host, '1')
+    redis_connect.lpush('free_process:' + host, i)
 
 # 主进程逻辑
-i = 0
 while True:
-    redis_connect.brpop('free_process:' + host)
+    # 接收新命令执行
+    if os.path.exists(cmd_file):
+        with open(cmd_file, 'r') as f:
+            cmd = f.readline().strip()
+            do_cmd(cmd)
+        os.remove(cmd_file)
+
+    sub_id = redis_connect.brpop('free_process:' + host)[1]
     url, deep = url_pop(host)
     if url is None:
-        # 任务全部做完，发送信号给子程序，并等待子进程结束
-        for j in range(process_num):
-            pipe_pool[j][0].send('---end---')
+        end_program()
 
-        process_pool.close()
-        process_pool.join()
-        print('All url done.')
-        exit()
-
-    pipe_num = i % process_num
     log('pipe_send', url + '------' + str(deep))
-    pipe_pool[pipe_num][0].send(url + '------' + str(deep))
-    i = i + 1
+    pipe_pool[int(sub_id)][0].send(url.strip("\n") + '------' + str(deep))
--- python_lab/test.py
+++ python_lab/test.py
@@ -4,9 +4,13 @@
 'test'
 __author__ = 'lockheed'
 
-process_num = 4
-i = 0
-while i < 20:
-    pipe_num = i % process_num
-    print(pipe_num)
-    i = i + 1
+import os
+import fcntl
+import time
+
+for i in range(1000):
+    with open('test.txt', 'a') as f:
+        # fcntl.flock(f, fcntl.LOCK_EX)
+        time.sleep(0.003)
+        f.write('1\n')
+        # fcntl.flock(f, fcntl.LOCK_UN)
