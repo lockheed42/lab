c39a20c999d0f368926bef7ca739dcb7781619fc
lockheed42 <hongjiexxx@126.com>
2019-01-15 17:49:11 +0800

    新Tushare接口

--- python_lab/turtle/catch.py
+++ python_lab/turtle/catch.py
@@ -60,23 +60,31 @@ def mysql_fetch(sql, fetchone=True):
 
 def catch_stock():
     """ 抓取所有股票代码 """
+    global token
+
     cdate = time.strftime('%Y-%m-%d %H:%M:%S')
-    rs = ts.get_industry_classified()
+    pro = ts.pro_api(token)
+    rs = pro.stock_basic(fields='symbol, name, industry, fullname, market, list_status, list_date, delist_date')
     for stock in rs.values:
-        sql = "SELECT * FROM src_industry WHERE name = '" + stock[2] + "'"
-        mysql_rs = mysql_fetch(sql)
-
         try:
-            if mysql_rs is None:
-                sql = "INSERT INTO src_industry (`name`, `cdate`) VALUES ('" + stock[2] + "', '" + cdate + "')"
-                mysql_rs = mysql_insert(sql)
-                industry_id = str(mysql_rs)
-            else:
-                industry_id = str(mysql_rs[0])
+            if stock[2] is not None:
+                sql = "SELECT * FROM src_industry WHERE name = '" + stock[2] + "'"
+                mysql_rs = mysql_fetch(sql)
 
-            insert_sql = "INSERT INTO src_stock (`code`, `name`, `industry_id`, `cdate`, `udate`)" \
-                         " VALUES ('" + str(stock[0]) + "','" \
-                         + str(stock[1]) + "' ," + industry_id + " ,'" + cdate + "' ,'" + cdate + "' )"
+                if mysql_rs is None:
+                    sql = "INSERT INTO src_industry (`name`, `cdate`) VALUES ('" + stock[2] + "', '" + cdate + "')"
+                    mysql_rs = mysql_insert(sql)
+                    industry_id = str(mysql_rs)
+                else:
+                    industry_id = str(mysql_rs[0])
+            else:
+                industry_id = 0
+
+            insert_sql = "INSERT INTO src_stock (`code`, `name`, `fullname`, `industry_id`, `market`, `status`," \
+                         " `list_date`, `delist_date`, `cdate`, `udate`) " \
+                         "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s','%s','%s','%s')" \
+                         % (stock[0], stock[1], stock[3], industry_id, stock[4], stock[5], stock[6], stock[7], cdate,
+                            cdate)
             mysql_insert(insert_sql)
         except BaseException as e:
             log('stock_error', str(stock[0]) + '|' + str(stock[1]) + '|' + str(traceback.format_exc()))
@@ -85,8 +93,11 @@ def catch_stock():
 
 def catch_daily_trade(stock_code, start, end):
     """ 抓取个股 日交易信息 """
+    global token
+
     try:
-        data = ts.get_k_data(stock_code, start=start, end=end)
+        pro = ts.pro_api(token)
+        data = pro.daily(ts_code=stock_code, start=start, end=end)
         counter = 0
         start_runtime = time.time()
 
@@ -94,8 +105,8 @@ def catch_daily_trade(stock_code, start, end):
         cdate = time.strftime('%Y-%m-%d %H:%M:%S')
         if data.empty is not True:
             for day in data.values:
-                insert_sql += "('" + day[6] + "','" + day[0] + "'," + str(day[1]) + "," + str(day[2]) \
-                              + "," + str(day[3]) + "," + str(day[4]) + "," + str(day[5]) + ",'" + cdate + "'),"
+                insert_sql += "('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'),"\
+                              % (day[0], day[1], day[2], day[5], day[3], day[4], day[9], cdate)
                 counter = counter + 1
             insert_sql = insert_sql.strip(',')
             mysql_insert(insert_sql)
@@ -122,15 +133,17 @@ def catch_daily_trade(stock_code, start, end):
 
 
 def init_all_day_trade():
-    """ 初始化所有股票 日交易数据"""
-    sql = "SELECT code from src_stock where `status` = 1"
+    """ 初始化所有股票 日交易数据。剔除不追踪的"""
+    sql = "SELECT code from src_stock where `is_trace` = 1"
     data = mysql_fetch(sql, False)
     for code in data:
         print(code[0])
         # 1990年有10只股票，而且有些已经不存在了，以此为起点
-        for year in range(1990, 2019):
-            catch_daily_trade(code[0], str(year) + '-01-01', str(year) + '-10-22')
+        catch_daily_trade(code[0], '19900101', '20191022')
 
 
 log_path = '/Library/WebServer/Documents/code/lab/python_lab/turtle/log'
+token = 'b122fa2788bd599ca9b5ae1b02a51fa0a5e4c7724e54de4955cb1c34'
 # init_all_day_trade()
+
+catch_daily_trade('600581.SH', '19900101', '20191022')
