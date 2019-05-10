d88f76f1c5dece37e7fc906dea383478ce2e8bf4
lockheed42 <hongjiexxx@126.com>
2019-05-10 15:16:38 +0800

    重做海龟

--- python_lab/turtle/model_ma.py
+++ python_lab/turtle/model_ma.py
@@ -28,6 +28,14 @@ class ModelMa(sim.Sim):
     # 记录K先是否在均线之下
     is_under_ma = False
 
+    # 20日最高价列表
+    max_20 = []
+    # 20日最高价
+    max_20_p = 0
+    # 10日最低价
+    min_10 = []
+    min_10_p = 0
+
     # 买入信号。创建时是因为买入点发现后的第二天操作，用于完全穿越K线
     buy_sign = False
     # 卖出信号。
@@ -64,16 +72,25 @@ class ModelMa(sim.Sim):
         self.is_under_ma = False
         self.buy_sign = False
         self.sell_sign = False
+        self.max_20 = []
+        self.max_20_p = 0
+        self.min_10 = []
+        self.min_10_p = 0
 
     def main_before(self, ids, code, date, open_p, close, high, low, vol, c_date):
         self.ma_20_p = self.get_ma(self.ma_20) if len(self.ma_20) == 20 else 99999
         # self.ma_30_p = self.get_ma(self.ma_30) if len(self.ma_30) == 30 else 99999
-        # self.ma_60_p = self.get_ma(self.ma_60) if len(self.ma_60) == 60 else 99999
-        # if self.ma_20:
-        #     self.max_20_p = max(self.ma_20)
-        # else:
-        #     self.max_20_p = 1
-        if self.have_status is False and self.ma_30_p > high:
+        self.ma_60_p = self.get_ma(self.ma_60) if len(self.ma_60) == 60 else 99999
+        if len(self.max_20) >= 20:
+            self.max_20_p = max(self.max_20)
+        else:
+            self.max_20_p = 999999
+
+        if len(self.min_10) >= 10:
+            self.min_10_p = min(self.min_10)
+        else:
+            self.min_10_p = 1
+        if self.have_status is False and self.ma_60_p > high:
             self.is_under_ma = True
 
     def main_buy(self, ids, code, date, open_p, close, high, low, vol, c_date):
@@ -84,12 +101,16 @@ class ModelMa(sim.Sim):
         #         # 未彻底破ma60前，高于ma20视为趋势延续，但此时必须 ma20高于ma60，防止错误信号
         #         (self.is_under_ma is False and self.ma_60_p < self.ma_20_p <= low)):
 
-        if self.buy_sign is False and self.have_status is False and low >= self.ma_30_p and self.is_under_ma is True:
-            self.buy_sign = True
-            return
+        # if self.buy_sign is False and self.have_status is False and low >= self.ma_30_p and self.is_under_ma is True:
+        #     self.buy_sign = True
+        #     return
 
         # 买入条件
-        if self.buy_sign is True:
+        if self.have_status is False and (
+                (self.ma_60_p <= high and self.is_under_ma is True and self.max_20_p <= high)
+                or
+                # 未彻底破ma60前，高于ma20视为趋势延续，但此时必须 ma20高于ma60，防止错误信号
+                (self.is_under_ma is False and self.ma_60_p <= self.ma_20_p <= high and self.max_20_p <= high)):
             # 涨停板无法买入
             if high == low:
                 return
@@ -97,15 +118,19 @@ class ModelMa(sim.Sim):
             self.buy_sign = False
 
             # TODO 买入价要经常思考是否可以更贴近实际
-            # if self.ma_20_p < self.ma_60_p:
-            #     self.buy_price = self.ma_60_p if open_p <= self.ma_60_p else open_p
-            # else:
-            #     self.buy_price = self.ma_20_p if open_p <= self.ma_20_p else open_p
-
-            self.buy_price = self.ma_30_p if open_p <= self.ma_30_p else open_p
+            if self.ma_60_p <= high and self.is_under_ma is True and self.max_20_p <= high:
+                if self.max_20_p > self.ma_60_p:
+                    self.buy_price = self.max_20_p
+                else:
+                    self.buy_price = self.ma_60_p if open_p <= self.ma_60_p else open_p
+            else:
+                if self.max_20_p > self.ma_20_p:
+                    self.buy_price = self.max_20_p
+                else:
+                    self.buy_price = self.ma_20_p if open_p <= self.ma_20_p else open_p
 
             # 计算止损价
-            self.stop_loss_price = self.buy_price * Decimal(0.92)
+            self.stop_loss_price = self.buy_price * Decimal(0.95)
             # 获取上次卖出后的现金
             if len(self.tmp_trade_record) != 0:
                 self.money = self.tmp_trade_record[len(self.tmp_trade_record) - 1]['after_money']
@@ -128,30 +153,38 @@ class ModelMa(sim.Sim):
 
             self.have_status = True
             self.is_just_have = True
+            # 有时候价格已经到了均线之上，但其他条件不满足买入，将延迟is_under_ma改为false为买入后
             self.is_under_ma = False
 
     def main_sell(self, ids, code, date, open_p, close, high, low, vol, c_date):
-        if high <= self.ma_30_p and self.sell_sign is False:
-            self.sell_sign = True
-            return
+        # if high <= self.ma_30_p and self.sell_sign is False:
+        #     self.sell_sign = True
+        #     return
 
-        if self.sell_sign is True:
+        if (self.ma_20_p >= low or self.ma_60_p >= low) and self.min_10_p >= low:
             # 跌停板板无法卖出
             if low == high:
                 return
 
-            self.sell_sign = False
-
             # 当开盘已经时不再卖点，过均线时卖出
-            # 当开盘已经在均线下，卖出价取 开盘和收盘平均价
-            sell_price = self.ma_30_p if open_p >= self.ma_30_p else open_p
+            # 当开盘已经在均线下，直接卖出
+            # TODO 卖出价要经常思考是否可以更贴近实际
+            if self.ma_20_p >= low:
+                if self.min_10_p <= self.ma_20_p:
+                    sell_price = self.min_10_p
+                else:
+                    sell_price = self.ma_20_p if open_p >= self.ma_20_p else open_p
+            else:
+                if self.min_10_p <= self.ma_60_p:
+                    sell_price = self.min_10_p
+                else:
+                    sell_price = self.ma_60_p if open_p >= self.ma_60_p else open_p
 
             self.sell(sell_price, date, self.have_day, self.max_draw_down, self.max_draw_down_day)
             self.have_status = False
             self.have_day = 0
 
     def lost_control(self, ids, code, date, open_p, close, high, low, vol, c_date):
-        return
         # 计算止损价，判断是否需要止损
         # 2ATR
         # stop_loss_price = self.buy_price - np.mean(tr_list) * 2
@@ -167,12 +200,19 @@ class ModelMa(sim.Sim):
             self.ma_20.pop(0)
         # if len(self.ma_30) >= 30:
         #     self.ma_30.pop(0)
-        # if len(self.ma_60) >= 60:
-        #     self.ma_60.pop(0)
+        if len(self.ma_60) >= 60:
+            self.ma_60.pop(0)
+
+        if len(self.max_20) >= 20:
+            self.max_20.pop(0)
+        if len(self.min_10) >= 10:
+            self.min_10.pop(0)
 
         self.ma_20.append(close)
         # self.ma_30.append(close)
-        # self.ma_60.append(close)
+        self.ma_60.append(close)
+        self.max_20.append(high)
+        self.min_10.append(low)
 
     def track_model(self, code, end_date, start_date='1990-01-01'):
         self.run_model = 'track'
@@ -181,12 +221,12 @@ class ModelMa(sim.Sim):
 
 if __name__ == '__main__':
     # main
-    model_code = 'ma-30'
+    model_code = 'ma-6020-max20-min10'
     # 测试单条
-    model = ModelMa()
-    # ModelMa().track_model('000001', '2019-02-27', '2003-01-01')
-    model.main('000001', model_code, '2019-02-27', '2003-01-01')
-    exit()
+    # model = ModelMa()
+    # # ModelMa().track_model('000001', '2019-02-27', '2003-01-01')
+    # model.main('000001', model_code, '2019-02-27', '2003-01-01')
+    # exit()
 
     # 多进程
     model = ModelMa()
